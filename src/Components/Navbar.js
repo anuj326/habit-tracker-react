edb2efde0d11c73461f8cad53aaa8329fe472a27
Anuj Sahu <anujsahu5227@gmail.com>
2023-05-23 14:27:54 +0530

    complete code

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="Navbar mb-4">
+    <nav className="navbar navbar-light justify-content-between" style={{backgroundColor: "#e3f2f2"}}>
+        <Link to="/" className='navbar-brand m-2 ms-5' > <h2>Habit-Tracker</h2></Link>
+<form action="" className="form-inline">
+    <Link to="/add-habit" className="btn btn-outline-success me-5 my-sm-0 m-2" type="submit" >Add Habits</Link>
+</form>
+    </nav>
+    <h1 className='text-center author'>Designed by Anuj Sahu</h1>
+    </div>
+  )
+}
+
+export default Navbar
