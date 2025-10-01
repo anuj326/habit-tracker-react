8068e048ad74662c5d04965c914e4e2eb058d280
anuj326 <ajterminal2416@gmail.com>
2025-10-01 13:29:39 +0530

    updated Navbar Logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="Navbar mb-4">
     <nav className="navbar navbar-light justify-content-between" style={{backgroundColor: "#e3f2f2"}}>
-        <Link to="/" className='navbar-brand m-2 ms-5' > <h2>Habit-Tracker</h2></Link>
+        <Link to="/" className='navbar-brand m-2 ms-5' > <h2>Habit Tracker Dev With CI/CD</h2></Link>
 <form action="" className="form-inline">
     <Link to="/add-habit" className="btn btn-outline-success me-5 my-sm-0 m-2" type="submit" >Add Habits</Link>
 </form>
